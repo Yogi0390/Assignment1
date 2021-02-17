d5c556b2b00de5e73b1a5ddb251e46aa51bf20fa
Yogi0390 <74300889+Yogi0390@users.noreply.github.com>
2021-02-17 16:30:13 +0530

    update hello.java
    added few lines of java code now

--- hello.java
+++ hello.java
@@ -1,2 +1,12 @@
 This is my first project in git
 Hello World
+
+##########################################
+public class pullcommand
+{
+  public static void main(String args[])
+{
+  system.out.println("This is a samlple program to check pull command in git");
+}
+}
+
